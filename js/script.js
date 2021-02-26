e2512b4f3ad935620695891ef19a03348865e3cc
Eduard Gorshkov <britvahp@gmail.com>
2021-02-26 16:55:33 +0400

    modal--consult

--- js/script.js
+++ js/script.js
@@ -5,4 +5,24 @@ window.addEventListener('DOMContentLoaded', () => {
   burger.addEventListener('click', () => {
     menu.classList.toggle('nav__ul--mobile-active');
   });
+
+  function modalShow(trigger, modal, close) {
+    const trigg = document.querySelector(trigger),
+          modal_ = document.querySelector(modal),
+          close_ = document.querySelector(close);
+    trigg.addEventListener('click', () => {
+      modal_.classList.add('modal--visible');
+    });
+    modal_.addEventListener('click', e => {
+      if (e.target.classList.contains('modal')) {
+        modal_.classList.remove('modal--visible');
+      }
+    });
+    close_.addEventListener('click', () => {
+      modal_.classList.remove('modal--visible');
+    });
+  }
+
+  modalShow('.special__button', '.modal--consult', '.modal__close--consult');
+
 });
